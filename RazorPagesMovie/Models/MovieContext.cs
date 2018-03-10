a456d48a922f67502430622c210b99c245f4878c
Mikołaj Bochajczuk mikolaj.bochajczuk@intive.com <mikolaj.bochajczuk@intive.com>
2018-03-10 17:05:24 +0100

    mvc tutorial app added

---
+++ RazorPagesMovie/Models/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿namespace RazorPagesMovie.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+
+        public DbSet<Worker> Worker { get; set; }
+    }
+}
---
+++ RazorPagesMovie/Models/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Worker : Person
+    {
+        public int ID { get; set; }
+
+        public String company { get; set; }
+
+        public Nullable<System.Double> wage { get; set; }
+
+        public virtual ICollection<System.Int32> favouriteNumber { get; set; }
+    }
+}
