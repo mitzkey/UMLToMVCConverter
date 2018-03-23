0b1287eb1d052b4a9de7cea60f0062b961a5390d
Mikołaj Bochajczuk mikolaj.bochajczuk@intive.com <mikolaj.bochajczuk@intive.com>
2018-03-23 12:45:52 +0100

    trying to build with script, still buggy, wip

--- UMLToMVCConverter/ProjectBuilder.cs
+++ UMLToMVCConverter/ProjectBuilder.cs
@@ -1,5 +1,7 @@
 ﻿namespace UMLToMVCConverter
 {
+    using System;
+    using System.Diagnostics;
     using System.IO;
     using System.Linq;
     using System.Reflection;
@@ -15,13 +17,44 @@
 
             var project = new Project(projectPath);
 
-            project.Build();
+            RunBuildScript(projectPath);
 
             var outputPath = Path.Combine(projectFolderPath, project.Properties.Single(p => p.Name == "OutputPath").EvaluatedValue);
 
             var assemblyPath = Path.Combine(outputPath, projectName + ".dll");
 
             return Assembly.LoadFrom(assemblyPath);
+        }
+
+        private static void RunBuildScript(string projectPath)
+        {
+            var scriptName = "run.bat";
+
+            File.WriteAllText(scriptName, $@"dotnet msbuild {projectPath}");
+            // Start the child process.
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = scriptName
+                }
+            };
+
+            // Redirect the output stream of the child process.
+            process.Start();
+
+            // Do not wait for the child process to exit before
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+
+            var output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine(output);
+            Console.ReadLine();
         }
     }
 }
