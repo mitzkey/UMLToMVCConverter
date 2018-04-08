3c8ebb2685027e7674ca4028791cb9b7f0e0f65d
Mikołaj Bochajczuk mikolaj.bochajczuk@intive.com <mikolaj.bochajczuk@intive.com>
2018-04-08 18:10:01 +0200

    Got rid of main gui

---
+++ UMLToMVCConverter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UMLToMVCConverter
+{
+    public static class Program
+    {
+        private const string TemporaryHardCodedDiagramPath =
+            @"C:\Users\mikolaj.bochajczuk\Desktop\priv\Praca Inżynierska\UMLToMVCConverter\Diagramy\MainDiagram.xml";
+        private const string TemporaryHardCodedMvcProjectPath =
+            @"C:\Users\mikolaj.bochajczuk\Desktop\priv\WebApplication1\WebApplication1";
+        private const string TemporaryHardCodedConnectionString =
+            @"Server=(localdb)\mssqllocaldb;Database=Default;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        public static void Main(string[] args)
+        {
+            var xmiPath = TemporaryHardCodedDiagramPath;
+            var mvcProjectFolderPath = TemporaryHardCodedMvcProjectPath;
+            var dbConnectionString = TemporaryHardCodedConnectionString;
+
+            var mvcProjectConfigurator = new MvcProjectConfigurator(mvcProjectFolderPath, dbConnectionString);
+            var cg = new DataModelGenerator(xmiPath, mvcProjectConfigurator);
+
+            cg.GenerateMvcFiles();
+        }
+    }
+}
