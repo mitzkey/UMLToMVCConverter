7c70209fc032e9a8a31d0c4d909840a5d47c3048
Mikołaj Bochajczuk mikolaj.bochajczuk@intive.com <mikolaj.bochajczuk@intive.com>
2018-04-08 17:59:41 +0200

    MigrationsService command line arguments cleanup

--- MigrationsService/Program.cs
+++ MigrationsService/Program.cs
@@ -6,19 +6,33 @@
     {
         public static void Main(string[] args)
         {
-            if (args.Length < 3)
+            if (args.Length < 4)
             {
                 throw new ArgumentException(
-                    "Not enough arguments, specify: <mvcProjectFolderPath> <migrationsNamespace> <mvcProjectAssemblyPath>");
+                    "Not enough arguments, specify: <command> <mvcProjectFolderPath> <migrationsNamespace> <mvcProjectAssemblyPath>" +
+                    "\ncommands: add-migration, update-database");
             }
 
-            var mvcProjectFolderPath = args[0];
-            var migrationsNamespace = args[1];
-            var mvcProjectAssemblyPath = args[2];
+            var command = args[0];
+            var mvcProjectFolderPath = args[1];
+            var migrationsNamespace = args[2];
+            var mvcProjectAssemblyPath = args[3];
 
             var migrationsService = new MigrationsService(mvcProjectAssemblyPath);
 
-            migrationsService.AddMigration(mvcProjectFolderPath, migrationsNamespace);
+            switch (command)
+            {
+                case "add-migration":
+                    var migrationAdded = migrationsService.AddMigration(mvcProjectFolderPath, migrationsNamespace);
+                    Console.WriteLine("Successfully added migration:\n" + migrationAdded);
+                    break;
+                case "update-database":
+                    migrationsService.RunMigration();
+                    Console.WriteLine("Successfully updated database");
+                    break;
+                default:
+                    throw new ArgumentException("Wrong command, either 'run-migration' or 'update-database' enabled");
+            }
         }
     }
 }
