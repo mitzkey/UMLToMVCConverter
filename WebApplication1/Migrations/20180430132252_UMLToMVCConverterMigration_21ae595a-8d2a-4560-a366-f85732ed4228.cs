6f7b56b203a2b8ff14db9c2925515c04a564c9fb
Mikołaj Bochajczuk mikolaj.bochajczuk@intive.com <mikolaj.bochajczuk@intive.com>
2018-04-30 18:14:53 +0200

    Migrations generated on the way

---
+++ WebApplication1/Migrations/20180430132252_UMLToMVCConverterMigration_21ae595a-8d2a-4560-a366-f85732ed4228.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Migrations
+{
+    public partial class UMLToMVCConverterMigration_21ae595a8d2a4560a366f85732ed4228 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
