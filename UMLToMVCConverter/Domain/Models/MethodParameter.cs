6bde3665f597c5ae8611a92eccf029bbcf6c87fe
Mikołaj Bochajczuk mikolaj.bochajczuk@intive.com <mikolaj.bochajczuk@intive.com>
2018-05-19 11:53:31 +0200

    Removed unused constructor & properties from MethodParameter

--- UMLToMVCConverter/Domain/Models/MethodParameter.cs
+++ UMLToMVCConverter/Domain/Models/MethodParameter.cs
@@ -1,8 +1,5 @@
 ﻿namespace UMLToMVCConverter.Domain.Models
 {
-    using System;
-    using System.Collections.Generic;
-
     public class MethodParameter
     {
         public TypeReference TypeReference { get; set; }
@@ -10,21 +7,6 @@
         public string Name { get; }
 
         public string ExtTypeName => this.TypeReference.Name;
-
-        public bool IsGeneric => this.TypeReference.IsGeneric;
-
-        public List<TypeReference> Generics => this.TypeReference.Generics;
-
-        public MethodParameter(
-            Type type,
-            string name,
-            bool isBasic,
-            bool isGeneric = false,
-            IEnumerable<TypeReference> generics = null)
-        {
-            this.Name = name;
-            this.TypeReference = new TypeReference(type, isBasic, isGeneric, generics);
-        }
 
         public MethodParameter(TypeReference parameterType, string name)
         {
